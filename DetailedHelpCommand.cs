97f276fda65cb9e83342f52bcffc1abee7a1d654
THECOTOFTEHAS\tsebr <tsebroevgeny@yandex.ru>
2022-11-21 12:16:28 +0300

    new

--- DetailedHelpCommand.cs
+++ DetailedHelpCommand.cs
@@ -3,22 +3,20 @@ using TaskDependencyManagement;
 
 public class DetailedHelpCommand : ConsoleCommand
 {
+    private readonly Lazy<ICommandsExecutor> executor;
+    private readonly TextWriter writer;
     public DetailedHelpCommand(IServiceLocation locator)
         : base("help", "help <command>      # prints help for <command>", locator)
     { }
 
     public override void Execute(string[] args)
     {
-        var commandName = args[0];
-        var command = Locator.Get<ICommandsExecutor>().FindCommandByName(commandName);
+        var commandName = args[1];
+        var command = executor.Value.FindCommandByName(commandName);
         var writer = Locator.Get<TextWriter>();
         if (command != null)
-        {
             writer.WriteLine(command.Help);
-        }
         else
-        {
             writer.WriteLine("Not a command " + commandName);
-        }
     }
 }
